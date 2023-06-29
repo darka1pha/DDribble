1b6fa2c4ef78813b88157a3b9f16c62fb9d573df
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-06-29 12:58:53 +0330

    add profile menu

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from './layout/navbar'
 export { default as Footer } from './layout/footer'
 export { default as AuthProviders } from './authProviders'
+export { default as ProfileMenu } from './profileMenu'
--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,5 +1,5 @@
 import { NavLinks } from '@/constants'
-import { AuthProviders } from '@/components'
+import { AuthProviders, ProfileMenu } from '@/components'
 import Image from 'next/image'
 import Link from 'next/link'
 import { getCurrentUser } from '@/lib/session'
@@ -23,15 +23,7 @@ const Navbar = async () => {
 			<div className='flexCenter gap-4'>
 				{session?.user ? (
 					<>
-						{session.user.image && (
-							<Image
-								src={session.user.image}
-								width={40}
-								height={40}
-								className='rounded-full'
-								alt={session.user.name}
-							/>
-						)}
+						<ProfileMenu session={session} />
 						<Link href={'/create-project'}>Share Work</Link>
 					</>
 				) : (
